6f7c980d4f535a4ce54d962afba1e1dc008ff599
Shilpa Thukral <shilpa.10@tcs.com>
2018-04-08 01:18:01 +0530

    My Project setup committed successfully

---
+++ shoppingbackend/src/main/java/net/thukral/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.thukral.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
